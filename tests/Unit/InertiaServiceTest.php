486ee27dfa41e209a677972689e15f0e8c081e4b
Aleš Pázner~ <pza2bj@bosch.com>
2024-11-07 20:24:37 +0100

    dummy test

---
+++ tests/Unit/InertiaServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PaznerA\NetteInertia\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class InertiaServiceTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
